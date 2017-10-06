7e9b4bdcc734ecd7b5d78ac5311a405d15c9ca35
yjyesha <yj.yesha@gmail.com>
2017-10-06 13:20:05 -0400

    Removed unnecessary

--- assignment/app.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by yeshajoshi on 7/17/2017.
- */
-require("./services/user.service.server");
-require("./services/website.service.server");
-require("./services/page.service.server");
-require("./services/widget.service.server");
